39dd27b4f6f44d9e5db7c963ccfd4c58cf7a4942
vikas-afk <71164380+vikas-afk@users.noreply.github.com>
2021-04-22 16:48:09 +0530

    Add files via upload

---
+++ MasterDetailUsingGridview/MasterDetailUsingGridview/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿
+namespace MasterDetailUsingGridview
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_submit = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_address = new System.Windows.Forms.TextBox();
+            this.txt_age = new System.Windows.Forms.TextBox();
+            this.txt_name = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_submit
+            // 
+            this.btn_submit.Location = new System.Drawing.Point(272, 182);
+            this.btn_submit.Name = "btn_submit";
+            this.btn_submit.Size = new System.Drawing.Size(94, 29);
+            this.btn_submit.TabIndex = 14;
+            this.btn_submit.Text = "Submit";
+            this.btn_submit.UseVisualStyleBackColor = true;
+            this.btn_submit.Click += new System.EventHandler(this.btn_submit_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(105, 152);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 20);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Address";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(105, 94);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 20);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Age";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(105, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 20);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Name";
+            // 
+            // txt_address
+            // 
+            this.txt_address.Location = new System.Drawing.Point(217, 149);
+            this.txt_address.Name = "txt_address";
+            this.txt_address.Size = new System.Drawing.Size(199, 27);
+            this.txt_address.TabIndex = 10;
+            // 
+            // txt_age
+            // 
+            this.txt_age.Location = new System.Drawing.Point(217, 91);
+            this.txt_age.Name = "txt_age";
+            this.txt_age.Size = new System.Drawing.Size(199, 27);
+            this.txt_age.TabIndex = 9;
+            // 
+            // txt_name
+            // 
+            this.txt_name.Location = new System.Drawing.Point(217, 31);
+            this.txt_name.Name = "txt_name";
+            this.txt_name.Size = new System.Drawing.Size(199, 27);
+            this.txt_name.TabIndex = 8;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 250);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 29;
+            this.dataGridView1.Size = new System.Drawing.Size(429, 188);
+            this.dataGridView1.TabIndex = 15;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(461, 250);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowHeadersWidth = 51;
+            this.dataGridView2.RowTemplate.Height = 29;
+            this.dataGridView2.Size = new System.Drawing.Size(429, 188);
+            this.dataGridView2.TabIndex = 16;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(105, 479);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 29);
+            this.button1.TabIndex = 18;
+            this.button1.Text = "Students";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(681, 470);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(172, 29);
+            this.button2.TabIndex = 19;
+            this.button2.Text = "Students Record";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(902, 690);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btn_submit);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txt_address);
+            this.Controls.Add(this.txt_age);
+            this.Controls.Add(this.txt_name);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btn_submit;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt_address;
+        private System.Windows.Forms.TextBox txt_age;
+        private System.Windows.Forms.TextBox txt_name;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ MasterDetailUsingGridview/MasterDetailUsingGridview/Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+namespace MasterDetailUsingGridview
+{
+    public partial class Form1 : Form
+    {
+
+        SqlDataAdapter dad1, dad2;
+        DataSet ds;
+        DataRelation drel;
+        //DataTable dt = new DataTable();
+        SqlConnection con = new SqlConnection(@"Data Source=LAPTOP-0B0HU93C\MSSQLSERVER2019;Initial Catalog=Practice;Integrated Security=True");
+        
+        public Form1()
+        {
+            InitializeComponent();
+      
+        }
+       
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+           /* DataTable dt_Employee = new DataTable();
+
+            // add column to datatable 
+
+            dt_Employee.Columns.Add("Emp_ID", typeof(int));
+
+            dt_Employee.Columns.Add("Emp_Name", typeof(string));
+            dt_Employee.Columns.Add("Emp_Salary", typeof(string));
+
+
+            //Child table
+
+            DataTable dt_Department = new DataTable();
+
+            dt_Department.Columns.Add("Emp_ID", typeof(int));
+            dt_Department.Columns.Add("Dept_ID", typeof(int));
+
+            dt_Department.Columns.Add("Dept_Name", typeof(string));
+
+
+            //Adding Rows
+
+            dt_Employee.Rows.Add(1, "kirit", "25000");
+
+            dt_Employee.Rows.Add(2, "pintu", "80000");
+
+            dt_Employee.Rows.Add(3, "Rahul", "54222");
+
+
+
+            // Department for kirit ID=1
+
+            dt_Department.Rows.Add(1, "01", "IT");
+
+
+
+            //Department for pintu ID=2
+
+            dt_Department.Rows.Add(2, "02", "Account");
+            //Department for Rahul ID=3
+
+            dt_Department.Rows.Add(3, "03", "Finance");
+
+
+            DataSet dsDataset = new DataSet();
+
+            //Add two DataTables  in Dataset
+
+
+            dsDataset.Tables.Add(dt_Employee);
+
+            dsDataset.Tables.Add(dt_Department);
+
+            DataRelation Datatablerelation = new DataRelation("Employee_Department", dsDataset.Tables[0].Columns[0], dsDataset.Tables[1].Columns[0], true);
+
+            dsDataset.Relations.Add(Datatablerelation);
+
+            dataGridView1.DataSource = dsDataset.Tables[0];
+*/
+        }
+
+        private void btn_submit_Click(object sender, EventArgs e)
+        {
+           // CreateNewRow();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            dad2 = new SqlDataAdapter("select * from StudentRecords", con);
+            ds = new DataSet();
+            dad2.Fill(ds);
+            dataGridView2.DataSource = ds.Tables[0];
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dad1 = new SqlDataAdapter("select * from AllStudent", con);
+            ds = new DataSet();
+            dad1.Fill(ds);
+            dataGridView1.DataSource = ds.Tables[0];
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            dad1 = new SqlDataAdapter("select * from AllStudent", con);
+            ds = new DataSet();
+            dad1.Fill(ds, "AllStudent");
+            dad1.SelectCommand.CommandText = "select * from StudentRecords";
+            dad1.Fill(ds, "StudentRecords");
+            drel = new DataRelation("All", ds.Tables[0].Columns[0], ds.Tables[1].Columns[0]);
+            ds.Relations.Add(drel);
+            //dataGridView3.DataSource = ds;
+        }
+
+        /*  public void CreateNewRow()
+          {
+              if (dt.Rows.Count <= 0)
+              {
+                  DataColumn dc1 = new DataColumn("Name", typeof(string));
+                  DataColumn dc2 = new DataColumn("Age", typeof(string));
+                  DataColumn dc3 = new DataColumn("Address", typeof(string));
+                  dt.Columns.Add(dc1);
+                  dt.Columns.Add(dc2);
+                  dt.Columns.Add(dc3);
+
+
+                  dt.Rows.Add(txt_name.Text, txt_age.Text, txt_address.Text);
+                  dataGridView1.DataSource = dt;
+              }
+
+              else
+              {
+                  dt.Rows.Add(txt_name.Text, txt_age.Text, txt_address.Text);
+                  dataGridView1.DataSource = dt;
+              }
+          }
+  */
+
+    }
+}
